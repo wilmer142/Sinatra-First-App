49d0ec827b14a3ba83fc783f6b36446583b66e7d
wilmer142 <wilmercollege@gmail.com>
2018-03-06 20:58:12 -0500

    Adding views and implementing boostrap for style

--- app.rb
+++ app.rb
@@ -1,5 +1,45 @@
 require 'sinatra'
+require 'faker'
+require 'pry'
+
+include Faker
+
 
 get '/' do
-	"hello world"
+	@characters = []
+	10.times do
+		@characters.push(DragonBall.character)
+	end
+	erb :index
+end
+
+
+=begin
+get '/' do
+	status 200
+	headers "Content-Type" => "text/html"
+	body "hola Mundo"
+end
+=end
+
+get '/greet' do
+	"Hola #{params[:name]} hoy es #{params[:dia]}"
+end
+
+get '/home' do
+	@params = {name: params[:name], dia: params[:dia], clase: params[:clase]}
+	erb :home
+end
+
+get '/contact/:name' do
+	@cualquiercosa = params[:name]
+	erb :contact
+end
+
+get '/contact' do
+	erb :contact
+end
+
+get '/about' do
+	erb :about
 end
